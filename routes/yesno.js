ca1bf57578e571895f21c2b64d7f25ee0903b44b
240bismuth2352 <2401100270sr@cyber-u.ac.jp>
2026-06-11 07:29:15 +0900

    add yesno

--- routes/yesno.js
+++ routes/yesno.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const request = require('request');
 
 router.get('/', async (req, res) => {
-request('https://yesno.wtf/assets/no/4-122be48db47678331dbef3dd6ac6ff56.gif', function (error, response, body) {
+request('https://yesno.wtf/api', function (error, response, body) {
 if (!error && response.statusCode == 200) {
 const data = JSON.parse(body);
 res.json(data);
