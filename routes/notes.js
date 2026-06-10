56d0e4d37b125134eb115cfc555dc8debb1dec4e
240bismuth2352 <2401100270sr@cyber-u.ac.jp>
2026-06-11 05:19:20 +0900

    change notes

--- routes/notes.js
+++ routes/notes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://2401100270cu:2401100270cu@cluster0.56gxr4f.mongodb.net/?appName=Cluster0";
+const uri = "*************";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
@@ -12,7 +12,7 @@ const database = client.db('notes');
 const notes = database.collection('notes');
 
 // idが１のドキュメントを取得
-const query = { id: 1 };
+const query = { id: 2 };
 const note = await notes.findOne(query);
 
 res.json(note);
